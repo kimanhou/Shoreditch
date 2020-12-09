2a6175e9268ad6a3d49aa59a58731033ad20a6fa
Kim Anh Nguyen <kimanhou@gmail.com>
2020-12-09 10:01:44 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App : React.FunctionComponent = props => {
+  return (
+    <div className={`App`}>
+      <h1>Shoreditch</h1>
+      <h2>By Maki</h2>
+    </div>
+  )
+}
+export default App;
