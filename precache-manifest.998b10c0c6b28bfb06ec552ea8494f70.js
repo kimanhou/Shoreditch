812e1e5650ce92351ff8b6bd86ecffb392d0674a
Kim Anh Nguyen <kimanhou@gmail.com>
2020-12-16 17:49:44 +0100

    Updates

---
+++ precache-manifest.998b10c0c6b28bfb06ec552ea8494f70.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "83ce9e105de86ad94ca0",
+    "url": "Shoreditch/static/css/main.49311742.chunk.css"
+  },
+  {
+    "revision": "83ce9e105de86ad94ca0",
+    "url": "Shoreditch/static/js/main.83ce9e10.chunk.js"
+  },
+  {
+    "revision": "8ab01008021f9d55754c",
+    "url": "Shoreditch/static/js/1.8ab01008.chunk.js"
+  },
+  {
+    "revision": "460e07cc1c58410c50b0",
+    "url": "Shoreditch/static/js/runtime~main.460e07cc.js"
+  },
+  {
+    "revision": "d254f533d9bb6fe4e84b07f21062381c",
+    "url": "Shoreditch/static/media/AnticDidone-Regular.d254f533.woff"
+  },
+  {
+    "revision": "09a2fa178cd3968fc819fe8ae210eb28",
+    "url": "Shoreditch/static/media/AmaticSC-Regular.09a2fa17.woff"
+  },
+  {
+    "revision": "8ba765ce9580a3bdae76b59130d0ef8a",
+    "url": "Shoreditch/static/media/AmaticSC-Bold.8ba765ce.ttf"
+  },
+  {
+    "revision": "e9a42e339f93a719c17be53c386dbbfb",
+    "url": "Shoreditch/static/media/AmaticSC-Regular.e9a42e33.ttf"
+  },
+  {
+    "revision": "e917b5a9dcd9a24157425a9aee6da804",
+    "url": "Shoreditch/static/media/AnticDidone-Regular.e917b5a9.ttf"
+  },
+  {
+    "revision": "b9de7658afae1fccab285540339f7b93",
+    "url": "Shoreditch/static/media/AmaticSC-Bold.b9de7658.woff"
+  },
+  {
+    "revision": "dd40f505e7756d8cadc37ade775839d2",
+    "url": "Shoreditch/static/media/ReenieBeanie-Regular.dd40f505.woff"
+  },
+  {
+    "revision": "dd40f505e7756d8cadc37ade775839d2",
+    "url": "Shoreditch/static/media/ReenieBeanie-Regular.dd40f505.ttf"
+  },
+  {
+    "revision": "c930b9f6513cabf84aebd8f1da858b55",
+    "url": "Shoreditch/static/media/brick.c930b9f6.jpg"
+  },
+  {
+    "revision": "a7e04368c58bafdee88b1c56e832b734",
+    "url": "Shoreditch/static/media/home.a7e04368.jpg"
+  },
+  {
+    "revision": "a47178f1dcbb7c22f43637c839aa2313",
+    "url": "Shoreditch/static/media/shoreditch-map.a47178f1.png"
+  },
+  {
+    "revision": "6ca7bdfedf962aa0039b89d7f7997883",
+    "url": "Shoreditch/index.html"
+  }
+];
