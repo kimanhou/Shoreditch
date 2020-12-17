a49ecc7ff3c4d94d518500eb5c65baca2f8a8856
Kim Anh Nguyen <kimanhou@gmail.com>
2020-12-17 15:17:06 +0100

    Transition Right photos

--- src/view/components/PlaceDetails/Right/Right.tsx
+++ src/view/components/PlaceDetails/Right/Right.tsx
@@ -8,10 +8,10 @@ interface IRightProps {
 }
 
 const Right : React.FC<IRightProps> = props => {
-
+    var visibleClassname = props.place != null ? 'visible' : '';
 
     return(
-        <div className={`place-details-right`}>
+        <div className={`place-details-right ${visibleClassname}`}>
             {props.place != null && 
                 <>
                     <RightPhoto caption='The food' photoCredits={props.place.getInstagram()} url={props.place.photoFoodUrl}/>
