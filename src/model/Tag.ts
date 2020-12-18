55e74236f3ce758f21ab609bd1a21a43880976ed
Kim Anh Nguyen <kimanhou@gmail.com>
2020-12-18 15:45:29 +0100

    Tags and update content

---
+++ src/model/Tag.ts
@@ -0,0 +1,30 @@
+export enum Tag {
+    // Ambiance
+    LOCALSONLY = 'Locals only',
+    POPULAR = 'Popular',
+    DATE = 'Perfect for dates',
+    SHARING = 'Perfect for sharing',
+    QUIRKY = 'Quirky',
+    FAST = 'Fast',
+    MARKET = 'Market stall',
+    INTIMATE = 'Intimate atmosphere',
+    TAKEAWAY = 'Take away',
+
+    // Food
+    PIZZA = 'Great pizza',
+    SUSHI = 'Great sushi',
+    INDIAN = 'Great Indian food',
+    BURGER = 'Great burgers',
+    ITALIAN = 'Great Italian food',
+    FRENCH = 'Great French food',
+    ASIAN = 'Great Asian food', // includes Burmese, Vietnamese, Thai, Chinese, Japanese
+
+    // Drink
+    BEER = 'Great beer',
+    COCKTAIL = 'Great cocktails',
+    PUB = 'Great pub',
+
+    // Budget
+    CHEAP = 'Cheap eats',
+    FANCY = 'Fancy eats'
+}
