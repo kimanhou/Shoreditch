dfcc7218a5efb5ab11d151b6ebb7dcadfffd4480
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-07 16:18:42 +0100

    Footer Social animation on hover

---
+++ src/view/components/Footer/Cross.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Cross.scss';
+
+interface ICrossProps {
+
+}
+
+const Cross : React.FC<ICrossProps> = props => {
+    return (
+        <svg className={`cross`} viewBox="0 0 8.4666665 8.4666669" >
+            <g>
+                <path className={`bar-1`}
+                    style={{ fill: 'none', 
+                                stroke: '#f9e44a', 
+                                strokeWidth: '0.52916667',
+                                strokeLinecap: 'butt',
+                                strokeLinejoin: 'miter',
+                                strokeMiterlimit: 4,
+                                strokeOpacity: 1,}}
+                    d="M 1.3229167,1.3229167 C 2.7815707,3.6812045 5.1021027,5.3265599 7.14375,7.14375" />
+                <path className={`bar-2`}
+                    style={{ fill: 'none', 
+                                stroke: '#f9e44a', 
+                                strokeWidth: '0.52916667',
+                                strokeLinecap: 'butt',
+                                strokeLinejoin: 'miter',
+                                strokeMiterlimit: 4,
+                                strokeOpacity: 1,}}
+                    d="M 7.1460906,1.3498509 C 4.7878028,2.8085049 3.1424474,5.129037 1.3252573,7.1706842" />
+            </g>
+        </svg>
+    );
+}
+
+export default Cross;
--- src/view/components/Footer/FooterSocial.tsx
+++ src/view/components/Footer/FooterSocial.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cross from './Cross';
 import './FooterSocial.scss';
 
 interface IFooterSocialProps {
@@ -10,6 +11,8 @@ const FooterSocial : React.FC<IFooterSocialProps> = props => {
 
     return(
         <div className={`footer-social ${props.classname}`}>
+            <div className={`footer-social-background-hover`}></div>
+            <Cross />
             <a href={props.link} target='_blank'/>
         </div>
     )
