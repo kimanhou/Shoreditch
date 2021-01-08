c30656dcf286ffce81c35ea8035460a9bec733f6
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-08 11:56:38 +0100

    Mobile version Questions

--- src/view/components/Home/HomePage.tsx
+++ src/view/components/Home/HomePage.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Home from './Home';
+import './HomePage.scss';
 
 const HomePage : React.FC = props => {
 
     return(
         <div className={`home-page`}>
-            <Header />
-            <Home />
+            <div className={`header-and-home`}>
+                <Header />
+                <Home />
+            </div>
             <Footer />
         </div>
     )
