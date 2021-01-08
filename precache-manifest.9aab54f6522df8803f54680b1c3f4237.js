6ccc8b567b19f18139da5164010d4e61db63439c
Kim Anh Nguyen <kimanhou@gmail.com>
2021-01-08 15:11:13 +0100

    Updates

--- precache-manifest.9aab54f6522df8803f54680b1c3f4237.js
+++ precache-manifest.9aab54f6522df8803f54680b1c3f4237.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8ec0341cc95afaffd098",
-    "url": "./static/css/main.3b015c22.chunk.css"
+    "revision": "0e24adb7db2fe7dfe76c",
+    "url": "./static/css/main.a92da706.chunk.css"
   },
   {
-    "revision": "8ec0341cc95afaffd098",
-    "url": "./static/js/main.8ec0341c.chunk.js"
+    "revision": "0e24adb7db2fe7dfe76c",
+    "url": "./static/js/main.0e24adb7.chunk.js"
   },
   {
     "revision": "795f29696add77c519c2",
@@ -20,40 +20,40 @@ self.__precacheManifest = [
     "url": "./static/media/AmaticSC-Bold.b9de7658.woff"
   },
   {
-    "revision": "e9a42e339f93a719c17be53c386dbbfb",
-    "url": "./static/media/AmaticSC-Regular.e9a42e33.ttf"
-  },
-  {
-    "revision": "e917b5a9dcd9a24157425a9aee6da804",
-    "url": "./static/media/AnticDidone-Regular.e917b5a9.ttf"
-  },
-  {
     "revision": "09a2fa178cd3968fc819fe8ae210eb28",
     "url": "./static/media/AmaticSC-Regular.09a2fa17.woff"
   },
   {
-    "revision": "d254f533d9bb6fe4e84b07f21062381c",
-    "url": "./static/media/AnticDidone-Regular.d254f533.woff"
+    "revision": "e9a42e339f93a719c17be53c386dbbfb",
+    "url": "./static/media/AmaticSC-Regular.e9a42e33.ttf"
   },
   {
     "revision": "8ba765ce9580a3bdae76b59130d0ef8a",
     "url": "./static/media/AmaticSC-Bold.8ba765ce.ttf"
   },
   {
+    "revision": "e917b5a9dcd9a24157425a9aee6da804",
+    "url": "./static/media/AnticDidone-Regular.e917b5a9.ttf"
+  },
+  {
     "revision": "dd40f505e7756d8cadc37ade775839d2",
     "url": "./static/media/ReenieBeanie-Regular.dd40f505.woff"
+  },
+  {
+    "revision": "d254f533d9bb6fe4e84b07f21062381c",
+    "url": "./static/media/AnticDidone-Regular.d254f533.woff"
   },
   {
     "revision": "dd40f505e7756d8cadc37ade775839d2",
     "url": "./static/media/ReenieBeanie-Regular.dd40f505.ttf"
   },
   {
-    "revision": "379acf3ab3e867305daa9b149aa95859",
-    "url": "./static/media/icon-instagram.379acf3a.svg"
-  },
-  {
     "revision": "9288fd6e95b63e37c6f2e10513fc2f8a",
     "url": "./static/media/icon-mail.9288fd6e.svg"
+  },
+  {
+    "revision": "379acf3ab3e867305daa9b149aa95859",
+    "url": "./static/media/icon-instagram.379acf3a.svg"
   },
   {
     "revision": "aec041ef2a70eb9a48ed8f3d7107a33d",
@@ -72,7 +72,7 @@ self.__precacheManifest = [
     "url": "./static/media/shoreditch-map.3a48f2ac.png"
   },
   {
-    "revision": "7646300d4c9489f13f859411cb373c55",
+    "revision": "61364b0247a18657c54ef3e1aff9d6d6",
     "url": "./index.html"
   }
 ];
